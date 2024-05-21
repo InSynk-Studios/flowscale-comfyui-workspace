be13b70ae04ddd2e509f99748c9e84203ef258d9
Manish <manishsdlp222@gmail.com>
2024-05-21 11:29:49 +0530

    add css class

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -23,11 +23,10 @@
 //   );
 // }
 
-
 import { useState } from "react";
 import { Button, TextInput } from "@primer/react";
 import { TriangleDownIcon, PlusIcon, FileIcon } from "@primer/octicons-react";
-import { MainDrawer } from "@/features/misc"
+import { MainDrawer } from "@/features/misc";
 
 type Route = "root" | "customNodes" | "recentFlows";
 
@@ -37,7 +36,7 @@ export default function App() {
   return (
     <div
       style={{
-        width: "100vh",
+        // width: "100vh",
         position: "absolute",
         top: 0,
         left: 0,
@@ -47,53 +46,15 @@ export default function App() {
       <div
         className="tw-class"
         style={{
-          padding: 2,
           position: "fixed",
           top: 0,
           left: 0,
           right: 0,
-          justifyContent: "space-between",
           display: "flex",
         }}
       >
-        <div>
-          <Button
-            size="small"
-            onClick={() => setRoute("recentFlows")}
-            style={{ borderColor: 'white', color: 'black' }}
-          >
-            <div>
-              <FileIcon size={16} />
-              <TriangleDownIcon size={16} />
-            </div>
-          </Button>
-          <Button
-            size="small"
-            style={{ borderColor: 'white', color: 'black' }}
-          >
-            <div style={{ paddingLeft: 2 }}>
-              <PlusIcon size={16}  />
-              <div  style={{ fontSize: 1 }}>
-                New
-              </div>
-            </div>
-          </Button>
-          <TextInput
-            variant="small"
-            placeholder="Workflow name"
-          />
-        </div>
-      </div>
-
-      {route === "recentFlows" && (
-        // <RecentFilesDrawer
-        //   onclose={() => setRoute("root")}
-        //   onClickNewFlow={() => {
-        //     setRoute("root");
-        //   }}
-        // />
         <MainDrawer />
-      )}
+      </div>
     </div>
   );
 }
